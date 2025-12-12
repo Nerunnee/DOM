de3b3074218b4cf64f8ac5aebe278ef2537444a4
norovsuren15 <e.norvoo@gmail.com>
2025-12-12 13:58:18 +0800

    leet bodlogo

---
+++ script7.js
@@ -0,0 +1,20 @@
+// Bodlogo 1.
+let celsius = 36.5;
+
+let kelvin = celsius + 273.15;
+
+let fahrenheit = celsius * 1.8 + 32;
+
+console.log("Bodlogo 1.", "kelvin:", kelvin, "fahrenheit:", fahrenheit);
+
+// Bodlogo 2.
+
+celsius = 122.11;
+
+kelvin = celsius + 273.15;
+fahrenheit = celsius * 1.8 + 32;
+
+console.log("Bodlogo 2.", "kelvin:", kelvin, "fahrenheit:", fahrenheit);
+
+// Bodlogo 3.
+
